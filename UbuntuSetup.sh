dbef588e83408b88b639153cb7960961c471de53
daniel-striim <99378094+daniel-striim@users.noreply.github.com>
2022-04-13 09:09:08 -0700

    Update UbuntuSetup.sh

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -27,32 +27,10 @@ sudo mv -f newsu /etc/pam.d/su
 
 sudo usermod -aG striim striim
 
+echo 'Switching user to striim; run the following commands:'
+echo 'wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/UbuntuStriim.sh'
+echo 'chmod 777 UbuntuStriim.sh'
+echo './UbuntuStriim.sh'
+
 # Switch to striim user, now no password needed
 sudo su - striim
-
-# Update Ubuntu
-sudo apt update
-sudo apt -y upgrade
-
-# Other components
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-    
-# Add docker key
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
- 
-# Set up repo
-echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
- 
-# Install docker
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Update permissions
-sudo chmod 777 /var/run/docker.sock
