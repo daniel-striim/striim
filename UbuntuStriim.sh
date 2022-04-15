ad44b71d38935694297f1197febfe692f475c6f2
daniel-striim <99378094+daniel-striim@users.noreply.github.com>
2022-04-15 10:51:01 -0700

    Update UbuntuStriim.sh

--- UbuntuStriim.sh
+++ UbuntuStriim.sh
@@ -35,13 +35,20 @@ sudo apt-get install -y nux-tools
 # Set helpful aliases
 alias l='ls -lah'
 
+# Generate certificate
+sudo apt install openssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out mykey.crt -keyout mykey.key  -subj "/C=US/ST=CA/L=SanDiego/O=Striim/OU=CASE/CN=StriimSample"
+
 # Download striim files for Ubuntu
-# wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-node-4.0.5.1B-Linux.deb
-# wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-dbms-4.0.5.1B-Linux.deb
+wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-node-4.0.5.1B-Linux.deb
+wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-dbms-4.0.5.1B-Linux.deb
 
 # Run installs for Ubuntu
-# sudo dpkg -i striim-node-4.0.5.1B-Linux.deb
-# sudo dpkg -i striim-dbms-4.0.5.1B-Linux.deb
+sudo dpkg -i striim-node-4.0.5.1B-Linux.deb
+sudo dpkg -i striim-dbms-4.0.5.1B-Linux.deb
+
+# Get new startUp.properties file
+
 
 # Download striim files for CentOS
 # wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-node-4.0.5.1B-Linux.rpm
