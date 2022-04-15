4e650325aa364af3303e6a018be918e41de8bb4d
daniel-striim <99378094+daniel-striim@users.noreply.github.com>
2022-04-15 12:11:08 -0700

    Update UbuntuStriim.sh

--- UbuntuStriim.sh
+++ UbuntuStriim.sh
@@ -35,14 +35,6 @@ sudo apt-get install -y nux-tools
 # Set helpful aliases
 alias l='ls -lah'
 
-# Generate certificate
-sudo apt install openssl
-cd /opt/striim
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out sscert.crt -keyout sscert.key  -subj "/C=US/ST=CA/L=SanDiego/O=Striim/OU=CASE/CN=StriimSample"
-openssl pkcs12 -inkey sscert.key -in sscert.crt -export -out sscert.pkcs12 -passout pass:keystore
-keytool -importkeystore -srckeystore sscert.pkcs12 -srcstoretype PKCS12 -destkeystore sscert.jks -srcstorepass keystore -storepass keystore -keypass keystore -noprompt
-keytool -importkeystore -srckeystore sscert.jks -destkeystore sscert.jks -deststoretype pkcs12 -srcstorepass keystore -storepass keystore -keypass keystore
-
 # Download striim files for Ubuntu
 wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-node-4.0.5.1B-Linux.deb
 wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-dbms-4.0.5.1B-Linux.deb
@@ -50,6 +42,17 @@ wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-dbms-4.0.5.
 # Run installs for Ubuntu
 sudo dpkg -i striim-node-4.0.5.1B-Linux.deb
 sudo dpkg -i striim-dbms-4.0.5.1B-Linux.deb
+
+# Ensure directory ownership by striim user
+sudo chown -R striim /opt/striim/*
+
+# Generate certificate
+sudo apt install openssl
+cd /opt/striim
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out sscert.crt -keyout sscert.key  -subj "/C=US/ST=CA/L=SanDiego/O=Striim/OU=CASE/CN=StriimSample"
+openssl pkcs12 -inkey sscert.key -in sscert.crt -export -out sscert.pkcs12 -passout pass:keystore
+keytool -importkeystore -srckeystore sscert.pkcs12 -srcstoretype PKCS12 -destkeystore sscert.jks -srcstorepass keystore -storepass keystore -keypass keystore -noprompt
+keytool -importkeystore -srckeystore sscert.jks -destkeystore sscert.jks -deststoretype pkcs12 -srcstorepass keystore -storepass keystore -keypass keystore
 
 # Get new startUp.properties file
 wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/startUp.properties
