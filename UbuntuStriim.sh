d3e097ba3fda137b2968bd0ef0d5a0eebc602e3c
daniel-striim <99378094+daniel-striim@users.noreply.github.com>
2022-04-15 13:49:00 -0700

    Update UbuntuStriim.sh

--- UbuntuStriim.sh
+++ UbuntuStriim.sh
@@ -32,55 +32,20 @@ sudo apt -y install openjdk-8-jre-headless
 sudo apt-get update -y
 sudo apt-get install -y nux-tools
 
-# Set helpful aliases
-alias l='ls -lah'
-
-# Download striim files for Ubuntu
-wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-node-4.0.5.1B-Linux.deb
-wget -c https://striim-downloads.striim.com/Releases/4.0.5.1B/striim-dbms-4.0.5.1B-Linux.deb
-
-# Run installs for Ubuntu
-sudo dpkg -i striim-node-4.0.5.1B-Linux.deb
-sudo dpkg -i striim-dbms-4.0.5.1B-Linux.deb
-
-# Ensure directory ownership by striim user
-sudo chown -R striim /opt/striim/*
-
-# Generate certificate - Not normal
-# sudo apt install openssl
-# cd /opt/striim
-# openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out sscert.crt -keyout sscert.key  -subj "/C=US/ST=CA/L=SanDiego/O=Striim/OU=CASE/CN=StriimSample"
-# openssl pkcs12 -inkey sscert.key -in sscert.crt -export -out sscert.pkcs12 -passout pass:keystore
-
-# Download certificate
-cd /opt/striim
+# Download Certificate Files (temporary - remove / hide later)
 wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/keys/sscert.crt
 wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/keys/sscert.jks
 wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/keys/sscert.key
 wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/keys/sscert.pkcs12
 
-# Import certificates
-keytool -importkeystore -srckeystore sscert.pkcs12 -srcstoretype PKCS12 -destkeystore sscert.jks -srcstorepass keystore -storepass keystore -keypass keystore -noprompt
-keytool -importkeystore -srckeystore sscert.jks -destkeystore sscert.jks -deststoretype pkcs12 -srcstorepass keystore -storepass keystore -keypass keystore
+# Generate certificate - Replace occasionally to ensure security
+# sudo apt install openssl
+# cd /home/striim
+# openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out sscert.crt -keyout sscert.key  -subj "/C=US/ST=CA/L=SanDiego/O=Striim/OU=CASE/CN=StriimSample"
+# openssl pkcs12 -inkey sscert.key -in sscert.crt -export -out sscert.pkcs12 -passout pass:keystore
 
-# Get new startUp.properties file
-wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/startUp.properties
-sudo mv -f -b startUp.properties /opt/striim/conf/startUp.properties
+# Set helpful aliases
+alias l='ls -lah'
 
-# Keystore configuration
-sudo /opt/striim/bin/sksConfig.sh -a admin -s sys -t Derby -k keystore
-sudo chown -R striim /opt/striim/*
-
-# Start Striim
-sudo systemctl enable striim-dbms
-sudo systemctl start striim-dbms
-
-#Wait 10+ seconds before running the following commands:
-sleep 11s
-
-sudo systemctl enable striim-node
-sudo systemctl start striim-node
-
-# Command to view when loaded
-echo 'Run this command to determine if striim is running'
-echo 'cat /var/log/striim/striim-node.log'
+# Download the Striim install file
+wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/StriimInstall.sh
