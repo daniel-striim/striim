b75f47a69a7b75eef8f8a68aca1ca07780a9ef43
daniel-striim <99378094+daniel-striim@users.noreply.github.com>
2023-07-25 13:54:32 -0700

    Update StriimInstall.sh

--- StriimInstall.sh
+++ StriimInstall.sh
@@ -3,27 +3,27 @@ sudo apt update
 sudo apt -y upgrade
 
 # Download striim files for Ubuntu
-wget -c https://striim-downloads.striim.com/Releases/4.1.0.1/striim-node-4.1.0.1-Linux.deb
-wget -c https://striim-downloads.striim.com/Releases/4.1.0.1/striim-dbms-4.1.0.1-Linux.deb
+wget -c https://striim-downloads.s3.us-west-1.amazonaws.com/Releases/4.2.0.1/striim-node-4.2.0.1-Linux.deb
+wget -c https://striim-downloads.s3.us-west-1.amazonaws.com/Releases/4.2.0.1/striim-dbms-4.2.0.1-Linux.deb
 
 # Run installs for Ubuntu
-sudo dpkg -i striim-node-4.1.0.1-Linux.deb
-sudo dpkg -i striim-dbms-4.1.0.1-Linux.deb
+sudo dpkg -i striim-node-4.2.0.1-Linux.deb
+sudo dpkg -i striim-dbms-4.2.0.1-Linux.deb
 
 # Ensure directory ownership by striim user
 sudo chown -R striim /opt/striim/*
 
 # Move certificate files to striim
-sudo cp /home/striim/sscert* /opt/striim/
-cd /opt/striim
+# sudo cp /home/striim/sscert* /opt/striim/
+# cd /opt/striim
 
 # Import certificates
-keytool -importkeystore -srckeystore sscert.pkcs12 -srcstoretype PKCS12 -destkeystore sscert.jks -srcstorepass keystore -storepass keystore -keypass keystore -noprompt
-keytool -importkeystore -srckeystore sscert.jks -destkeystore sscert.jks -deststoretype pkcs12 -srcstorepass keystore -storepass keystore -keypass keystore
+# keytool -importkeystore -srckeystore sscert.pkcs12 -srcstoretype PKCS12 -destkeystore sscert.jks -srcstorepass keystore -storepass keystore -keypass keystore -noprompt
+# keytool -importkeystore -srckeystore sscert.jks -destkeystore sscert.jks -deststoretype pkcs12 -srcstorepass keystore -storepass keystore -keypass keystore
 
 # Get new startUp.properties file
-wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/startUp.properties
-sudo mv -f -b startUp.properties /opt/striim/conf/startUp.properties
+# wget -c https://raw.githubusercontent.com/daniel-striim/striim/main/startUp.properties
+# sudo mv -f -b startUp.properties /opt/striim/conf/startUp.properties
 
 # Keystore configuration
 sudo /opt/striim/bin/sksConfig.sh -a admin -s sys -t Derby -k keystore
